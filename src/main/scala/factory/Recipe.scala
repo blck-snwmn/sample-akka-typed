bb6bece5d8a10412ab46cc57e03c0ca7f22db702
blck-snwmn <whitesnowmancreator@gmail.com>
2019-04-19 23:41:12 +0900

    update use recipe to create

--- src/main/scala/factory/Recipe.scala
+++ src/main/scala/factory/Recipe.scala
@@ -3,7 +3,7 @@ package factory
 import scala.concurrent.duration.FiniteDuration
 import scala.concurrent.duration._
 
-class Recipe(productionTime: FiniteDuration, materialNum: Int, productNum: Int) {
+class Recipe(val productionTime: FiniteDuration, val materialNum: Int, val productNum: Int) {
   require(productionTime <= 1.minute)
   require(materialNum > 0)
   require(productNum > 0)
